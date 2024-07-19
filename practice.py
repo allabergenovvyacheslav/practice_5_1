e7af14b916137ba756983aca419791b2747fbf57
Vyacheslav <slawushkin@mail.ru>
2024-07-19 16:06:40 +0300

    Update practice.py

--- practice.py
+++ practice.py
@@ -19,6 +19,10 @@ class User:
 
 if __name__ == '__main__':
     database = Database()
-    user = User(input('Введите логин: '), input('Введите пароль: '), input('Подтвердите пароль :'))
-    database.add_user(user.username, user.password)
-
+    while True:
+        choise = input('Приветствую! Выберите действие: \n1- Вход \n2 - Регистрация \n')
+        user = User(input('Введите логин: '), password := input('Введите пароль: '), password2 :=input('Подтвердите пароль :'))
+        if password != password2:
+            exit()
+        database.add_user(user.username, user.password)
+        print(database.data)
